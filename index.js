930e0e70b5b7d6276f9cf4c54263069989fdfa4e
Naomi Cole <naomisarahcole@gmail.com>
2022-05-31 11:20:27 +1000

    4 passing tests 5 to go

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name = "Ralph");
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name = "Bob");
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+};
+
+
+
+// cats.shift();
+
+
+  // console.log(cats);
