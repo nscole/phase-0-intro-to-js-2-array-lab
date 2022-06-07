5928306fa64b573d126024b4594bb2b1e2faeeed
Naomi Cole <naomisarahcole@gmail.com>
2022-06-07 12:10:16 +1000

    3 passing test phase-1

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ function destructivelyAppendCat(name){
     cats.push(name = 'Ralph');
 };
 
+function destructivelyPrependCat(name){
+    cats.unshift(name = 'Bob');
+};
 
+
+
+/* 
+describe('destructivelyPrependCat(name)', function () {
+    it('prepends a cat to the beginning of the cats array', function () {
+        destructivelyPrependCat("Bob");
+
+        expect(cats).to.have.ordered.members(["Bob", "Milo", "Otis", "Garfield"]);
+      });
+    });
+*/
