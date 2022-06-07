6f3c303d9fd38f4d141195adfa517a58a7e5bc2f
Naomi Cole <naomisarahcole@gmail.com>
2022-06-07 12:19:55 +1000

    Passing 7 tests Phase-1

--- index.js
+++ index.js
@@ -16,16 +16,20 @@ function destructivelyRemoveFirstCat(){
     cats.shift();
 };
 
-function appendCat(name){
+function appendCat(){
     let name_1 = ["Milo", "Otis", "Garfield", "Broom"];
         return name_1;
 };
 
+function prependCat(){
+    let name_2 = ["Arnold", "Milo", "Otis", "Garfield"];
+        return name_2;
+}
 
 /* 
-describe('appendCat(name)', function () {
-      it('appends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
-        expect(appendCat("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+describe('prependCat(name)', function () {
+      it('prepends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
+        expect(prependCat("Arnold")).to.have.ordered.members(["Arnold", "Milo", "Otis", "Garfield"]);
 
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
       });
